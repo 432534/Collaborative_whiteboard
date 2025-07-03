38a9facfa657b99e3e8f4071de9b316268943f96
adarsh <adarshsingh8853757189@gmail.com>
2025-07-04 01:45:44 +0530

    added build file

--- Whiteboard/server/server.js
+++ Whiteboard/server/server.js
@@ -3,27 +3,36 @@ const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*", 
     methods: ["GET", "POST"]
   }
 });
+
 app.use(cors());
 app.use(express.json());
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Whiteboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
 const roomRoutes = require('./routes/rooms');
 app.use('/api/rooms', roomRoutes);
+
 const socketHandler = require('./socket/socketHandler');
 socketHandler(io);
 
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
