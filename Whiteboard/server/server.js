62d90a4ea650c898134d2ecf2d45698b9b35de6e
adarsh <adarshsingh8853757189@gmail.com>
2025-07-04 15:19:24 +0530

    done some changes

--- Whiteboard/server/server.js
+++ Whiteboard/server/server.js
@@ -3,38 +3,26 @@ const http = require('http');
 const socketIo = require('socket.io');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"]
   }
 });
-
 app.use(cors());
 app.use(express.json());
-
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Whiteboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-const roomRoutes = require('./server/routes/rooms');
+const roomRoutes = require('./routes/rooms');
 app.use('/api/rooms', roomRoutes);
-
-const socketHandler = require('./server/socket/socketHandler');
+const socketHandler = require('./socket/socketHandler');
 socketHandler(io);
-
-// ---------- Serve React build ----------
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
