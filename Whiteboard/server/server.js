70ac16687c4ecae34a388dbc598c8ab78c052bb5
adarsh <adarshsingh8853757189@gmail.com>
2025-07-04 16:52:26 +0530

    done some changes

--- Whiteboard/server/server.js
+++ Whiteboard/server/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // DB connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
