be0eff5b3df51e73b693c20d24e03c7f9bf5cf66
adarsh <adarshsingh8853757189@gmail.com>
2025-07-04 15:52:44 +0530

    done some changes

--- Whiteboard/server/server.js
+++ Whiteboard/server/server.js
@@ -10,26 +10,37 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*", // during local dev
     methods: ["GET", "POST"]
   }
 });
+
+// Middlewares
 app.use(cors());
 app.use(express.json());
+
+// DB Connection
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/Whiteboard', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// API routes
 const roomRoutes = require('./routes/rooms');
 app.use('/api/rooms', roomRoutes);
+
+// Socket handler
 const socketHandler = require('./socket/socketHandler');
 socketHandler(io);
 
+// Serve frontend build (React)
 app.use(express.static(path.join(__dirname, '../client/build')));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
+// Start server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
